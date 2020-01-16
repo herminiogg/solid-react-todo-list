2df6accbec1eb3af19ad20c0df2aa86e5d585491
Herminio García <herminiogg@hotmail.com>
2020-01-16 11:53:51 +0100

    Updates

---
+++ precache-manifest.ae1a59e8cbce87d12d9f097db2844ac4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7b4a0de4ee9a8caa1b2b601525b23c",
+    "url": "/solid-react-todo-list/index.html"
+  },
+  {
+    "revision": "2b38ec256311e00a7803",
+    "url": "/solid-react-todo-list/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "849f3712f58409e6c284",
+    "url": "/solid-react-todo-list/static/js/2.5658e876.chunk.js"
+  },
+  {
+    "revision": "9914ebca252bc7ca8af740df5246766c",
+    "url": "/solid-react-todo-list/static/js/2.5658e876.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b38ec256311e00a7803",
+    "url": "/solid-react-todo-list/static/js/main.519889ac.chunk.js"
+  },
+  {
+    "revision": "d8bc17eb47b9627f8344",
+    "url": "/solid-react-todo-list/static/js/runtime-main.c8daeba0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/solid-react-todo-list/static/media/logo.ee7cd8ed.svg"
+  }
+]);
